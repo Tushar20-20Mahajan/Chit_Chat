9dbb40e15e380717ef6fadd6fa4169a26bd2d0bf
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-28 02:52:57 +0530

    Changes Done and Files Added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+// import React from 'react'
+
+// export const Chat = () => {
+//   return (
+//     <div className='chat'>Chat</div>
+//   )
+// }
+
+
+
+import React from "react";
+import Cam from "../assets/cam.png";
+import Add from "../assets/add.png";
+import More from "../assets/more.png";
+// import Messages from "./Messages";
+// import Input from "./Input";
+// import { ChatContext } from "../context/ChatContext";
 
 export const Chat = () => {
+  //const { data } = useContext(ChatContext);
+
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Tushar</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={Add} alt="" />
+          <img src={More} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
