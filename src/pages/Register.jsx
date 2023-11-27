c878a6c0a42d7c01a133bf150f21a729b92d012d
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-27 21:41:54 +0530

    Changes Done and Files Added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import logo from "../assets/logo.png"
 
 export const Register = () => {
   return (
    <div className='formContainer'>
     <div className='formWrapper'>
+    <img src={logo} style={{width:80 , height:60}} alt="Image Loading..." />
+        <span className='logo'>Chit-Chat</span>
+        <span className='title'>Register</span>
     <form>
         <input type='text' placeholder='Your Full Name'></input>
         <input type='email' placeholder='Your Email'></input>
         <input type='password' placeholder='Password'></input>
         <input type='file'></input>
+        <button>Sign-Up</button>
     </form>
+    <p>You do have an account? Login</p>
     </div>
    </div>
   )
