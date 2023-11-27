a8429042336a2aa7e38d69abf6f19029b15ad9e0
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-27 21:31:47 +0530

    Changes Done and Files Added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+   <div className='formContainer'>
+    <div className='formWrapper'>
+    <form>
+        <input type='text' placeholder='Your Full Name'></input>
+        <input type='email' placeholder='Your Email'></input>
+        <input type='password' placeholder='Password'></input>
+        <input type='file'></input>
+    </form>
+    </div>
+   </div>
   )
 }
