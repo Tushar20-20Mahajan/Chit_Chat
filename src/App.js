d35f3cc561ddf6460c9b48a8cc377630173628f1
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-27 21:24:45 +0530

    Changes Done and Files Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import { Register } from './pages/Register';
 
 function App() {
   return (
-    <div>
-      Hello World
-    </div>
+    <Register />
   );
 }
 
