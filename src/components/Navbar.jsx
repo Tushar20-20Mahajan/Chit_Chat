3bb4987bf59987615160a5d19607ec39c27119c6
Tushar Mahajan <tusharmahajan080@gmail.com>
2023-11-28 17:10:22 +0530

    Changes Done and Files Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import React, { useContext } from 'react'
 import {signOut} from "firebase/auth"
 import { auth } from '../firebase'
 import { AuthContext } from '../context/AuthContext'
+import logo from "../assets/logo.png"
 
 export const Navbar = () => {
   const {currentUser} = useContext(AuthContext)
 
   return (
     <div className='navbar'>
-      <span className="logo">Chit-Chat</span>
+      {/* <span className="logo">Chit-Chat</span> */}
+      <img src={logo} style={{ width: 90, height: 70 }} alt="Image Loading..." />
       <div className="user">
         <img src={currentUser.photoURL} alt="" />
         <span>{currentUser.displayName}</span>
